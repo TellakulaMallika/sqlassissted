21fe889069b6ab4e618602c2a1b609a8b9ef6c17
T.Mallika <tellakulamallika@gmailc.om>
2024-01-24 19:36:12 -0700

    mallika

---
+++ SQLQuery.sql
@@ -0,0 +1,5 @@
+use School
+  insert into Student (ID,name, class, address, email) values ('1','Tom', '7a', 'some address', 'tom@email.com');
+  insert into Student (ID,name, class, address, email) values ('2','Thomas', '7a', 'some address', 'thomas@email.com');
+  insert into Student(ID,name, class, address, email) values ('3','Manuel', '7a', 'some address', 'manuelm@email.com');
+select * from Student
